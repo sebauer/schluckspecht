953a9d3ec4536e48cc11f92fa9d9616f6bf16326
sebauer <gjlnetwork@googlemail.com>
2014-08-29 23:47:50 +0200

    Added test stubs

---
+++ server/test/service.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('bottleService', function(){
+  describe('addBottles', function() {
+    it('should return the new number of bottles on stock');
+    it('should increase the number of bottles of the given type by the number of bottles which have been added');
+    it('should fail if the number of added bottles is negative');
+  });
+});
