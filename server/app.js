95901af01e7f3f2b7ff7b2e9276fbb50833c1ec5
sebauer <gjlnetwork@googlemail.com>
2014-08-30 13:24:36 +0200

    Addded start script

--- server/app.js
+++ server/app.js
@@ -1,8 +1,30 @@
-var express = require('express');
+var mongoose = require('mongoose');
 var bunyan = require('bunyan');
+var restify = require('restify');
+var server = restify.createServer();
+
+var config = require('./config');
+var pjson = require('./package.json');
+var bottleService = require('./service/bottle-service');
 
 var log = bunyan.createLogger(
   {
     name: 'schluckspecht'
   }
 );
+
+log.info('Starting schluckspecht-server v%s', pjson.version);
+
+server.use(restify.bodyParser());
+
+log.info('Connecting with MongoDB..');
+db = mongoose.connect(config.mongooseConnectionString);
+bottleService.setConnection(db);
+
+server.post('/bottle-service/bottles/add/:id/:amount', function(req, res){
+  bottleService.addBottles(req.id, req.amount);
+});
+
+server.listen(config.restApiPort, function(){
+  log.info('Server started and listening on %s', server.url);
+});
