86c98dddd6c99790ad82855d6f759eb0c8e36131
sebauer <gjlnetwork@googlemail.com>
2014-08-31 16:49:12 +0200

    Added some documentation

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var bunyan = require('bunyan');
 var restify = require('restify');
 var server = restify.createServer();
+server.use(restify.bodyParser());
 
 var config = require('./config');
 var pjson = require('./package.json');
@@ -15,16 +16,16 @@ var log = bunyan.createLogger(
 
 log.info('Starting schluckspecht-server v%s', pjson.version);
 
-server.use(restify.bodyParser());
-
 log.info('Connecting with MongoDB..');
 db = mongoose.connect(config.mongooseConnectionString);
 
+// Set up routes
 bottleServiceRouter = new bottleServiceRouter(server, db, log);
 bottleServiceRouter.addRoute('/bottle-service/bottle-types/get', 'get');
 bottleServiceRouter.addRoute('/bottle-service/bottles/add', 'post');
 bottleServiceRouter.addRoute('/bottle-service/bottle-types/add', 'post');
 
+// Start server
 server.listen(config.restApiPort, function(){
   log.info('Server started and listening on %s', server.url);
 });
