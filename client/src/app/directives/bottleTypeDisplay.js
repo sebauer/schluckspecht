04102025343a7cf46e627a9f538d1a1583b65bbc
Markus Feindler <markus.feindler@inovex.de>
2014-09-04 19:26:50 +0200

    implemented bottle service directive

---
+++ client/src/app/directives/bottleTypeDisplay.js
@@ -0,0 +1,29 @@
+angular.module('schluckspecht.bottleTypeDisplay', [
+  'schluckspecht.bottleService'
+])
+
+.directive("bottleTypeDisplay", function() {
+  return {
+    scope: {
+      bottleType: '='
+    },
+    restrict: 'E',
+    replace: true,
+    templateUrl: 'directives/bottleTypeDisplay.tpl.html',
+    controller: ['$scope', 'bottleService', function($scope, bottleService) {
+      var bottleType = $scope.bottleType;
+      $scope.num = 0;
+
+      $scope.addBottles = function(num) {
+        bottleService.addBottles(bottleType.id, Number(num)).then(function(newStockCount) {
+          bottleType.stockCount = newStockCount;
+        }, function() {
+          alert("fail!");
+        });
+      };
+
+    }]
+  };
+})
+
+;
