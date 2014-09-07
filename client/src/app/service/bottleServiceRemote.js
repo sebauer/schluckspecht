62efadc713e9ece3421423b0e009eb4210f6abca
Markus Feindler <markus.feindler@inovex.de>
2014-09-07 17:44:31 +0200

    working rest service client

--- client/src/app/service/bottleServiceRemote.js
+++ client/src/app/service/bottleServiceRemote.js
@@ -5,16 +5,28 @@ angular.module('schluckspecht.bottleServiceRemote', [])
     var bottleTypesEndpoint = endpoint + "/bottle-types";
     var bottlesEndpoint = endpoint + "/bottles";
 
-    this.getBottleTypes = function() {
-      return $http.get(bottleTypesEndpoint + '/get');
+      this.getBottleTypes = function() {
+      return $http.get(bottleTypesEndpoint + '/get')
+        .then(defaultResponseHandler);
     };
 
     this.addBottles = function(bottleTypeId, num) {
-      return $http.post(bottlesEndpoint + '/add', {bottleTypeId: bottleTypeId, amount: num});
+      return $http.post(
+        bottlesEndpoint + '/add',
+        {bottleTypeId: bottleTypeId, amount: num}
+      ).then(defaultResponseHandler);
     };
 
     this.addBottleType = function(name, make) {
-      return $http.post(bottleTypesEndpoint + '/add', {name: name, make: make});
+      return $http.post(
+        bottleTypesEndpoint + '/add',
+        {name: name, make: make}
+      ).then(defaultResponseHandler);
+    };
+
+    var defaultResponseHandler = function(response) {
+      // this will make response.data to be passed to other "thens"
+      return response.data;
     };
 
   });
