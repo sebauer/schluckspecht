9a38b48bd1bd08b1be1f4d557f12ea606f6e19cf
Markus Feindler <markus.feindler@inovex.de>
2014-09-13 14:53:19 +0200

    starts local http with app configured and rest server

--- client/server.js
+++ client/server.js
@@ -1,12 +1,54 @@
-var express = require('express');
+var connect = require('connect');
 var http = require('http');
-var app = express();
+var serveStatic = require('serve-static');
+var proxy = require('proxy-middleware');
+var url = require('url');
+var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
+var config = require('./acceptance-test.config');
 
-app.use(express.static(__dirname + '/build'));
+// console.log(process.cwd());
 
-if (!module.parent) {
-  http.createServer(app).listen(process.env.PORT, function(){
-    console.log("Server listening on port " + app.get('port'));
+var restServer;
+
+var app = connect()
+  //.use(connect.logger('dev'))
+  .use(config.restAPIEndpoint, proxy(url.parse('http://localhost:' + config.restApiPort + '/')))
+  .use(serveStatic('build'))
+  //.use(connect.directory('public'))
+  .use(function(req, res){
+    res.end('Hello from Connect!\n');
   });
-}
-module.exports = app;
+
+http.createServer(app).listen(config.testWebServerPort, function() {
+  console.log("Base Server started");
+  startRestServer();
+});
+
+var startRestServer = function() {
+  // start REST-Server here
+  console.log("Trying to start REST Server");
+
+  restServer = spawn(config.startRestServerCmd.cmd, config.startRestServerCmd.params);
+
+  console.log("REST Server pid is " + restServer.pid);
+
+  restServer.stdout.on('data', function(data) {
+    var buff = new Buffer(data);
+    var line = buff.toString("UTF-8");
+
+    console.log(line);
+  });
+
+  restServer.stderr.on('data', function(data) {
+    var buff = new Buffer(data);
+    var line = buff.toString("UTF-8");
+
+    console.log(line);
+  });
+
+  restServer.on('close', function(code) {
+    console.log("close received for rest server");
+    restServer = null;
+  });
+};
